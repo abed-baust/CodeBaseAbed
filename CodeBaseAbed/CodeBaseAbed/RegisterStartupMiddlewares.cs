544ddf1f2c5c642544eb72d4065742c59e9bdadb
Abedur <abed.baust@gmail.com>
2022-08-15 16:20:42 +0600

    [Startup.cs] [ Clean way to add Startup class in ASP.NET Core 6 ]

---
+++ CodeBaseAbed/CodeBaseAbed/RegisterStartupMiddlewares.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeBaseAbed
+{
+    public static class RegisterStartupMiddlewares
+    {
+        public static WebApplication SetupMiddleware(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
---
+++ CodeBaseAbed/CodeBaseAbed/RegisterStartupServices.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeBaseAbed
+{
+    public static class RegisterStartupServices
+    {
+        public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            return builder;
+        }
+    }
+}
