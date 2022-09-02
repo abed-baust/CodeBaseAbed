cc8fb0a947edcd0de4b367cc55e085722aba5e99
Abedur <abed.baust@gmail.com>
2022-09-03 01:25:37 +0600

    [Dapper and SQL with minimal API]

---
+++ CodeBaseAbed/UserDB/dbo/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@Id int
+AS
+begin
+	DELETE 
+	FROM DBO.[User]
+	WHERE Id = @Id;
+end
---
+++ CodeBaseAbed/UserDB/dbo/StoredProcedures/spUser_Get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Get]
+	@Id int
+AS
+begin
+	SELECT *
+	FROM DBO.[User]
+	WHERE Id = @Id;
+end
