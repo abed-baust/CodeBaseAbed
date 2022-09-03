02c6ab1b7aea568c8878988eb405eb643f20ee22
Abedur <abed.baust@gmail.com>
2022-09-03 20:32:44 +0600

    [StoredProcedure]

---
+++ CodeBaseAbed/UserDB/obj/Debug/postdeploy.sql
@@ -0,0 +1,10 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName,LastName)
+	values ('Tim','corey'),
+		('Sue','corey'),
+		('rim','corey'),
+		('kim','corey'),
+		('Pim','corey');
+end
+GO
