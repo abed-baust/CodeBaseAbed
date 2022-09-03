110576fae53ef6f1f8bc236901d96fe549a19157
Abedur <abed.baust@gmail.com>
2022-09-04 00:43:17 +0600

    [Minimal API in .NET 6 and controllerbased Using Dapper and SQL]

--- CodeBaseAbed/CodeBaseAbed/Api.cs
+++ CodeBaseAbed/CodeBaseAbed/Api.cs
@@ -1,10 +1,16 @@
-﻿namespace CodeBaseAbed
+﻿using DataAccess.Models;
+
+namespace CodeBaseAbed
 {
     public static class Api
     {
         public static void ConfigureApi(this WebApplication app)
         {
-            app.MapGet("/Users1",GetUsers);
+            app.MapGet("/UsersMinimalApi",GetUsers);
+            app.MapGet("/UsersMinimalApi/{id}", GetUser);
+            app.MapPost("/UsersMinimalApi", InsertUser);
+            app.MapPut("/UsersMinimalApi", UpdateUser);
+            app.MapDelete("/UsersMinimalApi/{id}", DeleteUser);
         }
 
         private static async Task<IResult>GetUsers(IUserData data)
@@ -12,6 +18,59 @@
             try
             {
                 return Results.Ok(await data.GetUsers());
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> GetUser(int id,IUserData data)
+        {
+            try
+            {
+                var results = await data.GetUser(id);
+                if(results == null) return Results.NotFound();
+                return Results.Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+        {
+            try
+            {
+               await data.InserUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+        {
+            try
+            {
+                await data.UpdateUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> DeleteUser(int id, IUserData data)
+        {
+            try
+            {
+                await data.DeleteUser(id);
+                return Results.Ok();
             }
             catch (Exception ex)
             {
